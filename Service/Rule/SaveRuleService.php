b38748bfdac55c29396f69e1023c783b80b01c41
Yuriy Tkachenko <yuriit@fisha.co.il>
2022-05-15 13:09:27 +0300

    Code Review Stage 2

---
+++ Service/Rule/SaveRuleService.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Onestic\AcmeShipping\Service\Rule;
+
+use Onestic\AcmeShipping\Api\Service\Rule\SaveRulesServiceInterface;
+use Onestic\AcmeShipping\Api\Service\Vendor;
+
+class SaveRuleService implements SaveRulesServiceInterface
+{
+    /**
+     * @param array $rules
+     * @return void
+     */
+    public function execute(array $rules): void
+    {
+        // TODO: Implement execute() method.
+    }
+
+}
